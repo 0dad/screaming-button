231b51568cbf6b52fe6741428946b1ad8883681c
0xIan <32253127+ani9k@users.noreply.github.com>
2019-05-01 09:07:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function incrementParkinsonsOnJim(){
+	// on button press make the nigger shake fast
+	let ourBoys = document.getElementsByClassName("jim");
+	ourBoys.setAttribute("style", "animation:shake 1s");
+}
+
+
+function buttonPress(){
+	// incrementParkinsonsOnJim();
+	console.log("Button Press");
+	var x = document.getElementById("scream" + getRandomInt(8));
+	x.play();
+}
+
+function NO(){
+	console.log("fuckme");
+	// incrementParkinsonsOnJim();
+	let counter = 0;
+	let howManyLoops = 0;
+	let bigowie = document.getElementById("bigscream").play();
+	while(counter <= 7){
+		var x1 = document.getElementById("scream" + counter);
+		x1.play();
+		counter++;
+		if(howManyLoops < 2){
+			continue;
+		}else{
+			break;
+		}
+	}
+	howManyLoops++;
+
+}
+
